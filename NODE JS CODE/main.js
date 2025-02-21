2e6e2005957dccfa31ac18ab68a4c7df6a4860aa
VENISHETTY VEDAVYAS <151992063+vedavyasv@users.noreply.github.com>
2025-02-21 13:18:57 +0530

    Update main.js

--- NODE JS CODE/main.js
+++ NODE JS CODE/main.js
@@ -1,9 +1,5 @@
 const { processTestCase } = require("./utils");
-
-// Process both test cases
 const secret1 = processTestCase("testcase1.json");
 const secret2 = processTestCase("testcase2.json");
-
-// Output results
 console.log(`Secret for Test Case 1: ${secret1}`);
 console.log(`Secret for Test Case 2: ${secret2}`);
